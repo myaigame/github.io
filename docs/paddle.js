f6c7e46354b28f7cd22089c2f064afdaeaaf33c1
myaigame <162026960+myaigame@users.noreply.github.com>
2024-03-10 22:10:36 +0900

    Update paddle.js

--- docs/paddle.js
+++ docs/paddle.js
@@ -1,17 +1,17 @@
 export class Paddle {
-    constructor(ctx, width, height, x, canvasWidth, ballRadius) {
+    constructor(ctx, width, height, x, canvasWidth) {
         this.ctx = ctx;
         this.width = width * 2; // 너비를 2배로 조정
         this.height = height;
         this.x = x;
         this.canvasWidth = canvasWidth;
-        this.ballRadius = ballRadius; // 공의 지름 추가
+        this.gapFromBottom = 21; // 바닥에서 21픽셀 띄우기
     }
 
     draw() {
         this.ctx.beginPath();
-        // 패들 위치를 공의 지름만큼 바닥에서 띄우기
-        this.ctx.rect(this.x, this.ctx.canvas.height - this.height - this.ballRadius, this.width, this.height);
+        // 패들의 y 위치를 바닥에서 21픽셀 띄운 위치로 조정
+        this.ctx.rect(this.x, this.ctx.canvas.height - this.height - this.gapFromBottom, this.width, this.height);
         this.ctx.fillStyle = "#0095DD";
         this.ctx.fill();
         this.ctx.closePath();
