e753757675d6c8ce41c7f710c4dd4d9bca6c9a16
myaigame <162026960+myaigame@users.noreply.github.com>
2024-03-13 21:57:33 +0900

    Delete docs directory

--- docs/gameOver.js
+++
@@ -1,7 +0,0 @@
-export function gameOver(ctx, canvas) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // 캔버스 클리어
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.textAlign = "center";
-    ctx.fillText("Game Over", canvas.width / 2, canvas.height / 2); // 중앙에 게임 오버 메시지 표시
-}
